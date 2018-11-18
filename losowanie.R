1c6a72352ffadcdc5b9a378951f4efa6bc312718
kSzpyt <karolo.szpyt@gmail.com>
2018-11-18 01:38:01 +0100

    V1.0

--- losowanie.R
+++ losowanie.R
@@ -4,8 +4,8 @@ gen <- function(y)
   dane <- lapply(l, function(x)
   {
     list("ts" = rt(x, 2), 
-         "unif" = runif(x, -1, 1), 
-         "exp" = rexp(x, 10000)
+         "unif" = runif(x, -5, 5), 
+         "exp" = rexp(x, 1)
     )
     
   }) 
